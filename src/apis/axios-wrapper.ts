e0455c3f23856e0aa9be14ae162e2a80b997d218
hdkim <eastshine94@naver.com>
2022-10-18 11:14:41 +0900

    feat: axios-wrapper

---
+++ src/apis/axios-wrapper.ts
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import type { AxiosRequestConfig, AxiosError, AxiosResponse } from 'axios';
+import qs from 'qs';
+
+const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://dev.api.com'; // 현재 임의
+const commonConfigs: AxiosRequestConfig = {
+  baseURL,
+  timeout: 10000, // ms, default 0 = no timeout
+  transitional: { clarifyTimeoutError: true }, // ECONNABORTED -> ETIMEDOUT
+  paramsSerializer: {
+    encode: (params) => {
+      return qs.stringify(params);
+    },
+  },
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+// axios instance handling auth automatically with interceptro
+// -----------------------------------------------------------------
+export const client = axios.create(commonConfigs);
+
+client.interceptors.request.use(onBeforeRequest, onRequestError);
+client.interceptors.response.use(onBeforeResponse, onResponseError);
+
+async function onBeforeRequest(config: AxiosRequestConfig) {
+  console.log('- [axios] onBeforeRequest', config.url);
+
+  return config;
+}
+
+function onRequestError(error: AxiosError) {
+  console.log('- [client] onRequestError', error);
+  return Promise.reject(error);
+}
+
+function onBeforeResponse(response: AxiosResponse) {
+  console.log('- [axios] onBeforeResponse');
+  return response;
+}
+
+const EXPECTED_ERRORS = [400, 401, 404, 500];
+async function onResponseError(error: AxiosError) {
+  console.log('- [axios] onResponseError');
+  if (error.code === 'ETIMEDOUT') {
+    console.log('e onResponseError E_TIMEOUT');
+    return Promise.reject(error);
+  }
+  if (!error.response) {
+    console.log('e onResponseError E_NO_RESPONSE');
+    return Promise.reject(error);
+  }
+  if (EXPECTED_ERRORS.includes(error.response.status)) {
+    console.log(`e onResponseError E_${error.response.status}`);
+    return Promise.reject(error);
+  }
+
+  console.log('- onResponseError: unexpected', error?.response.status);
+  return Promise.reject(error);
+}
